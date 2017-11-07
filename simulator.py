391c03877f56991b6bebf7560c7568e9335d24f6
Rusheng <zrs1990@gmail.com>
2017-11-06 22:45:51 -0500

    some new stuff

--- simulator.py
+++ simulator.py
@@ -4,17 +4,26 @@ import random
 from time import time
 
 class Simulator():
-    def __init__(self, visual = False):
+    def __init__(self, visual = False, map_file = 'map/traffic.net.xml', route_file = 'map/traffic.rou.xml', end_time = 500, additional_file = None):
         self.visual = visual
+        self.map_file = map_file
+        self.end_time = end_time
+        self.route_file = route_file
+        self.additional_file = additional_file
         if self.visual == False:
- 	
-        
+            self.cmd = ['sumo', 
+                  '--net-file', self.map_file, 
+                  '--route-files', self.route_file,
+                  '--end', str(self.end_time)]
+            if not additional_file == None:      
+                self.cmd.append('--additional-files', self.additional_file)
+                
+    def simulation_start(self):
+        if self.visual == False:
+            pysumo.simulation_start(self.cmd)
+            return
 
-cmd = ['sumo', 
-  '--net-file', 'simple/traffic.net.xml', 
-  '--route-files', 'simple/traffic.rou.xml',
-  '--additional-files', 'simple/traffic.add.xml',
-  '--end', '500']
+
 actions = ['rGrG','ryry','GrGr','yryr']
 def random_action():
 	return random.choice(actions)
